963e0a6b7592f21f569f1b520806feb97466427b
Bagrat Grigoryan <bagrat.grigoryan@softconstruct.com>
2023-01-09 17:19:39 +0400

    deleted

--- my-up/src/components/header/Header.js
+++ my-up/src/components/header/Header.js
@@ -1,40 +1,9 @@
 import React from 'react';
-import {AppBar, Box, Toolbar, Typography} from "@mui/material";
-import { useNavigate } from "react-router-dom";
-function Header(props) {
-  const navigate = useNavigate();
-    return (
-        <Box sx={{flexGrow: 1}}>
-            <AppBar  style={{
-                background: "magenta"
-            }}>
-                <Toolbar>
-                    <Typography style={{
-                        border: "1px solid white",
-                        borderRadius: "25px",
-                        textAlign: "center",
-                        width: "50px",
-                        height: "50px",
-                        background: "orange",
-                        cursor:"pointer"
-                    }}
-                                component={"span"}
-                                variant={"h6"}
 
-                                onClick={() => {
-                                    navigate("/cms")
-                                }}
-                    >
-                     UP
-                    </Typography>
-                    <Typography style={{
-                        marginLeft: "10px"
-                    }}>
-                        Travel
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </Box>
+
+function Header(props) {
+    return(
+        <></>
     );
 }
 
